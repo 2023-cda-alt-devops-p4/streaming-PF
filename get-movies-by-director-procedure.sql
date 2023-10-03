113abd4f96757f699ac4628d74e3667d72bbbacf
PierreFrs <pierrefraisse90@gmail.com>
2023-10-03 18:47:40 +0200

    feat(get-movies-by-director.sql): Procedure creation
    Created procedure to get movies directed by a director in new
    get-movies-by-director.sql file

---
+++ get-movies-by-director-procedure.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE sp_GetMoviesByDirector
+@DirectorFirstName NVARCHAR(50),
+@DirectorLastName NVARCHAR(50)
+AS
+BEGIN
+    SELECT m.title
+    FROM movies m
+    JOIN directs d ON m.Id_movies = d.Id_movies
+    JOIN directors dir ON d.Id_directors = dir.Id_directors
+    WHERE dir.firstName = @DirectorFirstName AND dir.lastName = @DirectorLastName;
+END;
