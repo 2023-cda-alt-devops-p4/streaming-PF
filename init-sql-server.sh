0c06f1f68cbab9209d1aaa91e63c6b92faf1e729
PierreFrs <pierrefraisse90@gmail.com>
2023-10-16 10:22:01 +0200

    feat(Sql script): Changed order of scripts
    Changed the order of sql files run in script

--- init-sql-server.sh
+++ init-sql-server.sh
@@ -7,17 +7,18 @@
 sleep 30s
 
 # Run the SQL scripts against the database
-## The initialization script
 # path to sqlcmd utility that should connect to localhost server (-S) with the SA username (-U) and its password (-P) on the master database (-d) and run (-i) the specified file
+## The privileges script
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "${SA_PASSWORD}" -d master -i /sqlscripts/privileges.sql
+## The initialization script
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "${SA_PASSWORD}" -d master -i /sqlscripts/init.sql
 ## The trigger scripts
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "${SA_PASSWORD}" -d master -i /sqlscripts/triggers.sql
-## The population script
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "${SA_PASSWORD}" -d master -i /sqlscripts/data.sql
-## The privileges script
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "${SA_PASSWORD}" -d master -i /sqlscripts/privileges.sql
 ## The stored procedure script
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "${SA_PASSWORD}" -d master -i /sqlscripts/sp_GetMoviesByDirector.sql
+## The population script
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "${SA_PASSWORD}" -d master -i /sqlscripts/data.sql
+
 
 
 
